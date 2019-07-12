6875cff0c63820f603bc3583a791080b1eefb66f
Dellean Santos <dellean.santos@gmail.com>
2019-07-12 16:21:39 -0300

    prettier

--- axio-client.js
+++ axio-client.js
@@ -9,12 +9,12 @@ const client = axios.create({
   //keepAlive pools and reuses TCP connections, so it's faster
   httpAgent: new http.Agent({ keepAlive: true }),
   httpsAgent: new https.Agent({ keepAlive: true }),
-  
+
   //follow up to 10 HTTP 3xx redirects
   maxRedirects: 10,
-  
+
   //cap the maximum content length we'll accept to 50MBs, just in case
-  maxContentLength: 50 * 1000 * 1000
+  maxContentLength: 50 * 1000 * 1000,
 })
 
 module.exports = client
--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const Telegram = require('./telegram')
 
-
 const token = '856565326:AAFk5l23CC_OUk3pUOSoGKeUiDj_StpzLjs'
 const telegraClient = new Telegram({ token })
-
-
 
 telegraClient.lauch()
